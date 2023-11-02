2a5f86f55ede703bcdeccffb03f1928b980ebdb6
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2023-11-02 08:07:54 +0100

    First draft.

---
+++ ptyx_mcq_editor/editor.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+import sys
+
+from PyQt6.Qsci import QsciScintilla
+from PyQt6.QtGui import QFont, QColor
+from PyQt6.QtWidgets import QMainWindow, QFrame, QVBoxLayout, QPushButton, QApplication, QStyleFactory
+
+from lexer import MyLexer
+
+
+class CustomMainWindow(QMainWindow):
+    def __init__(self):
+        super(CustomMainWindow, self).__init__()
+
+        # -------------------------------- #
+        #           Window setup           #
+        # -------------------------------- #
+
+        # 1. Define the geometry of the main window
+        # ------------------------------------------
+        self.setGeometry(300, 300, 800, 400)
+        self.setWindowTitle("QScintilla Test")
+
+        # 2. Create frame and layout
+        # ---------------------------
+        self.__frm = QFrame(self)
+        self.__frm.setStyleSheet("QWidget { background-color: #ffeaeaea }")
+        self.__lyt = QVBoxLayout()
+        self.__frm.setLayout(self.__lyt)
+        self.setCentralWidget(self.__frm)
+        self.__myFont = QFont()
+        self.__myFont.setPointSize(14)
+
+        # 3. Place a button
+        # ------------------
+        self.__btn = QPushButton("Qsci")
+        self.__btn.setFixedWidth(50)
+        self.__btn.setFixedHeight(50)
+        self.__btn.clicked.connect(self.__btn_action)
+        self.__btn.setFont(self.__myFont)
+        self.__lyt.addWidget(self.__btn)
+
+        # -------------------------------- #
+        #     QScintilla editor setup      #
+        # -------------------------------- #
+
+        # ! Make instance of QSciScintilla class!
+        # ----------------------------------------
+        self.__editor = QsciScintilla()
+        self.__editor.setText("hello")     # 'myCodeSample' is a string containing some C-code
+        self.__editor.setLexer(None)            # We install lexer later
+        self.__editor.setUtf8(True)             # Set encoding to UTF-8
+        self.__editor.setFont(self.__myFont)    # Gets overridden by lexer later on
+
+        # 1. Text wrapping
+        # -----------------
+        self.__editor.setWrapMode(QsciScintilla.WrapMode.WrapWord)
+        self.__editor.setWrapVisualFlags(QsciScintilla.WrapVisualFlag.WrapFlagByText)
+        self.__editor.setWrapIndentMode(QsciScintilla.WrapIndentMode.WrapIndentIndented)
+
+        # 2. End-of-line mode
+        # --------------------
+        self.__editor.setEolMode(QsciScintilla.EolMode.EolUnix)
+        self.__editor.setEolVisibility(False)
+
+        # 3. Indentation
+        # ---------------
+        self.__editor.setIndentationsUseTabs(False)
+        self.__editor.setTabWidth(4)
+        self.__editor.setIndentationGuides(True)
+        self.__editor.setTabIndents(True)
+        self.__editor.setAutoIndent(True)
+
+        # 4. Caret
+        # ---------
+        self.__editor.setCaretForegroundColor(QColor("#ff0000ff"))
+        self.__editor.setCaretLineVisible(True)
+        self.__editor.setCaretLineBackgroundColor(QColor("#1f0000ff"))
+        self.__editor.setCaretWidth(2)
+
+        # 5. Margins
+        # -----------
+        # Margin 0 = Line nr margin
+        self.__editor.setMarginType(0, QsciScintilla.MarginType.NumberMargin)
+        self.__editor.setMarginWidth(0, "0000")
+        self.__editor.setMarginsForegroundColor(QColor("#ff888888"))
+
+        # -------------------------------- #
+        #          Install lexer           #
+        # -------------------------------- #
+        self.__lexer = MyLexer(self.__editor)
+        self.__editor.setLexer(self.__lexer)
+
+        # ! Add editor to layout !
+        # -------------------------
+        self.__lyt.addWidget(self.__editor)
+        self.show()
+
+    def __btn_action(self):
+        print("Hello World!")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
+    myGUI = CustomMainWindow()
+
+    sys.exit(app.exec())
