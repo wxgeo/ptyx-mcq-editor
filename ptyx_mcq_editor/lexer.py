2ec97956a7b881013db6b5c013c8ce27557016a1
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2023-11-03 16:48:10 +0100

    Add detection of python strings.

--- ptyx_mcq_editor/lexer.py
+++ ptyx_mcq_editor/lexer.py
@@ -27,24 +27,45 @@ ALL_BUILTINS = PYTHON_BUILTINS | PTYX_BUILTINS
 
 class Style(IntEnum):
     DEFAULT = 0
+
     PTYX_TAG = auto()
     PTYX_VARIABLE = auto()
     PTYX_EXPRESSION = auto()
     PTYX_COMMENT = auto()
+    PTYX_LATEX_MACRO = auto()
+
     PYTHON_BLOCK = auto()
     PYTHON_INT = auto()
     PYTHON_STR = auto()
     PYTHON_KEYWORD = auto()
     PYTHON_BUILTIN = auto()
     PYTHON_COMMENT = auto()
+
+    PYTHON_SINGLE_QUOTE_STRING = auto()
+    PYTHON_DOUBLE_QUOTE_STRING = auto()
+    PYTHON_TRIPLE_DOUBLE_QUOTE_STRING = auto()
+    PYTHON_TRIPLE_SINGLE_QUOTE_STRING = auto()
+    PYTHON_UNCLOSED_STRING = auto()
+
     MCQ_CORRECT_ANSWER = auto()
     MCQ_INCORRECT_ANSWER = auto()
     MCQ_NEUTRALIZED_ANSWER = auto()
 
 
+QUOTES = {
+    Style.PYTHON_SINGLE_QUOTE_STRING: "'",
+    Style.PYTHON_DOUBLE_QUOTE_STRING: '"',
+    Style.PYTHON_TRIPLE_SINGLE_QUOTE_STRING: "'''",
+    Style.PYTHON_TRIPLE_DOUBLE_QUOTE_STRING: '"""',
+}
+
+REVERSED_QUOTES = {val: key for key, val in QUOTES.items()}
+
+
 class Mode(Enum):
     DEFAULT = auto()
     PYTHON = auto()
+    PYTHON_STRING = auto()
     EXPRESSION = auto()
 
 
@@ -54,39 +75,40 @@ class StyleInfo(NamedTuple):
     paper_color: str
     is_bold: bool
     is_italic: bool
+    fill_line: bool
 
 
 STYLES_LIST: dict[Style, StyleInfo] = {
-    Style.DEFAULT: StyleInfo(Mode.DEFAULT, "#000000", "#ffffff", False, False),  # default
-    Style.PTYX_TAG: StyleInfo(Mode.DEFAULT, "#ffb230", "#ffffff", False, False),  # ptyx tag
-    Style.PTYX_VARIABLE: StyleInfo(Mode.DEFAULT, "#776599", "#ffffff", False, False),  # ptyx variable
-    Style.PTYX_EXPRESSION: StyleInfo(Mode.EXPRESSION, "#000000", "#fcf5bb", False, False),  # ptyx expression
-    Style.PTYX_COMMENT: StyleInfo(Mode.DEFAULT, "#cccccc", "#ffffff", False, True),  # ptyx comment
-    Style.PYTHON_BLOCK: StyleInfo(Mode.PYTHON, "#000000", "#fffad1", False, False),  # python block
-    Style.PYTHON_INT: StyleInfo(Mode.PYTHON, "#0000bf", "#fffad1", False, False),  # python int
-    Style.PYTHON_STR: StyleInfo(Mode.PYTHON, "#007f00", "#fffad1", False, False),  # python str
-    Style.PYTHON_KEYWORD: StyleInfo(Mode.PYTHON, "#ffb230", "#fffad1", True, False),  # python keyword
-    Style.PYTHON_BUILTIN: StyleInfo(Mode.PYTHON, "#ffb230", "#fffad1", False, False),  # python func
-    Style.PYTHON_COMMENT: StyleInfo(Mode.PYTHON, "#cccccc", "#fffad1", True, False),  # python comment
-    Style.MCQ_CORRECT_ANSWER: StyleInfo(Mode.DEFAULT, "#000000", "#d7ffd1", False, False),  # correct answer
-    Style.MCQ_INCORRECT_ANSWER: StyleInfo(
-        Mode.DEFAULT, "#000000", "#fffad1", False, False
-    ),  # incorrect answer
-    Style.MCQ_NEUTRALIZED_ANSWER: StyleInfo(
-        Mode.DEFAULT, "#000000", "#ebebeb", False, False
-    ),  # neutralized answer
+    # pTyX styles
+    Style.DEFAULT: StyleInfo(Mode.DEFAULT, "#000000", "#ffffff", False, False, False),
+    Style.PTYX_TAG: StyleInfo(Mode.DEFAULT, "#ffb230", "#ffffff", False, False, False),
+    Style.PTYX_VARIABLE: StyleInfo(Mode.DEFAULT, "#776599", "#ffffff", False, False, False),
+    Style.PTYX_LATEX_MACRO: StyleInfo(Mode.DEFAULT, "#753527", "#ffffff", False, False, False),
+    Style.PTYX_COMMENT: StyleInfo(Mode.DEFAULT, "#cccccc", "#ffffff", False, True, False),
+    # Expressions (small inline snippets of python code inside pTyX code)
+    Style.PTYX_EXPRESSION: StyleInfo(Mode.EXPRESSION, "#000000", "#fcf5bb", False, False, False),
+    # Python code
+    Style.PYTHON_BLOCK: StyleInfo(Mode.PYTHON, "#000000", "#fcf9d9", False, False, True),
+    Style.PYTHON_INT: StyleInfo(Mode.PYTHON, "#0000bf", "#fffad1", False, False, False),
+    Style.PYTHON_STR: StyleInfo(Mode.PYTHON, "#007f00", "#fffad1", False, False, False),
+    Style.PYTHON_KEYWORD: StyleInfo(Mode.PYTHON, "#ffb230", "#fffad1", True, False, False),
+    Style.PYTHON_BUILTIN: StyleInfo(Mode.PYTHON, "#cc12ff", "#fffad1", False, False, False),
+    Style.PYTHON_COMMENT: StyleInfo(Mode.PYTHON, "#cccccc", "#fffad1", True, False, False),
+    # Python strings
+    Style.PYTHON_SINGLE_QUOTE_STRING: StyleInfo(Mode.PYTHON_STRING, "#43b064", "#fffad1", False, False, True),
+    Style.PYTHON_DOUBLE_QUOTE_STRING: StyleInfo(Mode.PYTHON_STRING, "#43b064", "#fffad1", False, False, True),
+    Style.PYTHON_TRIPLE_DOUBLE_QUOTE_STRING: StyleInfo(
+        Mode.PYTHON_STRING, "#43b064", "#fffad1", False, False, True
+    ),
+    Style.PYTHON_TRIPLE_SINGLE_QUOTE_STRING: StyleInfo(
+        Mode.PYTHON_STRING, "#43b064", "#ffd7d1", False, False, True
+    ),
+    Style.PYTHON_UNCLOSED_STRING: StyleInfo(Mode.PYTHON_STRING, "#43b064", "#ffd7d1", False, False, True),
+    # MCQ specific syntax
+    Style.MCQ_CORRECT_ANSWER: StyleInfo(Mode.DEFAULT, "#138f00", "#d7ffd1", False, False, True),
+    Style.MCQ_INCORRECT_ANSWER: StyleInfo(Mode.DEFAULT, "#ab1600", "#ffd7d1", False, False, True),
+    Style.MCQ_NEUTRALIZED_ANSWER: StyleInfo(Mode.DEFAULT, "#707070", "#c2c2c2", False, False, True),
 }
-
-# self.setColor(QColor("#ff000000"), 0)  # default: black
-# self.setColor(QColor("#ffb230"), 1)  # ptyx tag: orange
-# self.setColor(QColor("#776599"), 2)  # ptyx variable: violet gris
-# self.setColor(QColor("#cccccc"), 3)  # ptyx comment: gray
-# self.setColor(QColor("#ff007f00"), 4)  # python block: green
-# self.setColor(QColor("#0000bf"), 5)  # python int: blue
-# self.setColor(QColor("#ff007f00"), 6)  # python str: green
-# self.setColor(QColor("#ff007f00"), 7)  # python func: green
-# self.setColor(QColor("#ff007f00"), 8)  # correct answer: green
-# self.setColor(QColor("#ff007f00"), 9)  # incorrect answer: green
 
 
 class MyLexer(QsciLexerCustom):
@@ -98,7 +120,7 @@ class MyLexer(QsciLexerCustom):
         self.setDefaultPaper(QColor("#ffffffff"))
         self.setDefaultFont(QFont("Consolas", 14))
 
-        for style, (_, text_color, paper_color, is_bold, is_italic) in STYLES_LIST.items():
+        for style, (_, text_color, paper_color, is_bold, is_italic, fill_line) in STYLES_LIST.items():
             self.setColor(QColor(text_color), style)
             self.setPaper(QColor(paper_color), style)
             self.setFont(
@@ -110,6 +132,7 @@ class MyLexer(QsciLexerCustom):
                 ),
                 style,
             )
+            self.setEolFill(fill_line, style)
 
     def language(self):
         return "PTYX MCQ"
@@ -133,7 +156,10 @@ class MyLexer(QsciLexerCustom):
         # 3. Tokenize the text
         # ---------------------
         # Test in https://regex101.com/
-        p = re.compile(r"\.{4,}\n|#\w+|#\{|^[-+!] |\s+|\w+\d*|\.\d+|\d+\.?|\W", flags=re.MULTILINE)
+        p = re.compile(
+            r"""\.{4,}\n|#\w+|#\{|^[-+!] |\\\\|\\'|\\"|\\[a-zA-Z]+|'{3}|"{3}|\s+|\w+\d*|\.\d+|\d+\.?|\W""",
+            flags=re.MULTILINE,
+        )
 
         # 'token_list' is a list of tuples: (token_name, token_len)
         token_list: list[tuple[str, int]] = [
@@ -142,58 +168,78 @@ class MyLexer(QsciLexerCustom):
 
         # 4. Style the text
         # ------------------
-        # 4.1 Get previous mode if any.
+        # 4.1 Get previous style and mode if any.
         if start > 0:
-            previous_style = editor.SendScintilla(editor.SCI_GETSTYLEAT, start - 1)
-            mode = STYLES_LIST[previous_style].mode
+            style = Style(editor.SendScintilla(editor.SCI_GETSTYLEAT, start - 1))
+            mode = STYLES_LIST[style].mode
         else:
+            style = Style.DEFAULT
             mode = Mode.DEFAULT
         # 4.2 Style the text in a loop
         for i, (token, length) in enumerate(token_list):
             assert isinstance(token, str) and isinstance(length, int), (token, length)
-            if token.startswith("...."):
-                self.setStyling(length, Style.PYTHON_BLOCK)
-                assert token.rstrip("\n").rstrip(".") == "", token
-                mode = Mode.PYTHON if mode == Mode.DEFAULT else Mode.DEFAULT
-            elif token == "#PYTHON":
-                self.setStyling(length, Style.PYTHON_BLOCK)
-                mode = mode.PYTHON
-            elif token == "#END_PYTHON" or (token == "#END" and mode == Mode.PYTHON):
-                self.setStyling(length, Style.PYTHON_BLOCK)
-                mode = mode.DEFAULT
-            elif mode == Mode.PYTHON:
-                if iskeyword(token):
-                    self.setStyling(length, Style.PYTHON_KEYWORD)
-                elif token in ALL_BUILTINS:
-                    self.setStyling(length, Style.PYTHON_BUILTIN)
-                elif token.strip(".").isdigit():
-                    self.setStyling(length, Style.PYTHON_INT)
-                else:
-                    self.setStyling(length, Style.PYTHON_BLOCK)
-            elif mode == Mode.EXPRESSION:
-                if token == "}":
-                    mode = mode.DEFAULT
-                    self.setStyling(length, Style.PTYX_TAG)
-                else:
-                    self.setStyling(length, Style.PTYX_EXPRESSION)
+            style, mode = self.get_style_and_mode(token, mode, style)
+            self.setStyling(length, style)
+
+    @staticmethod
+    def get_style_and_mode(token: str, mode: Mode, style: Style) -> tuple[Style, Mode]:
+        if mode == Mode.PYTHON_STRING:
+            if token == "\n" and style in (
+                Style.PYTHON_SINGLE_QUOTE_STRING,
+                Style.PYTHON_DOUBLE_QUOTE_STRING,
+            ):
+                mode = Mode.PYTHON
+                style = Style.PYTHON_UNCLOSED_STRING
+                mode = Mode.PYTHON
+            elif token == QUOTES[style]:
+                mode = Mode.PYTHON
+                # No need to change style.
+        elif token.startswith("...."):
+            style = Style.PYTHON_BLOCK
+            assert token.rstrip("\n").rstrip(".") == "", token
+            mode = Mode.PYTHON if mode == Mode.DEFAULT else Mode.DEFAULT
+        elif token == "#PYTHON":
+            style = Style.PYTHON_BLOCK
+            mode = mode.PYTHON
+        elif token == "#END_PYTHON" or (token == "#END" and mode == Mode.PYTHON):
+            style = Style.PYTHON_BLOCK
+            mode = mode.DEFAULT
+        elif token == "}" and mode == Mode.EXPRESSION:
+            mode = mode.DEFAULT
+            style = Style.PTYX_TAG
+        elif mode in (Mode.PYTHON, mode.EXPRESSION):
+            if token in REVERSED_QUOTES:
+                style = REVERSED_QUOTES[token]
+                mode = Mode.PYTHON_STRING
+            elif iskeyword(token):
+                style = Style.PYTHON_KEYWORD
+            elif token in ALL_BUILTINS:
+                style = Style.PYTHON_BUILTIN
+            elif token.strip(".").isdigit():
+                style = Style.PYTHON_INT
             else:
-                # print(repr(token))
-                if token.startswith("#"):
-                    if token in TAGS:
-                        self.setStyling(length, Style.PTYX_TAG)
-                    elif token == "#{":
-                        mode = Mode.EXPRESSION
-                        self.setStyling(length, Style.PTYX_TAG)
-                    elif token.startswith("#") and token[1:].isalpha():
-                        self.setStyling(length, Style.PTYX_VARIABLE)
-                    else:
-                        self.setStyling(length, Style.DEFAULT)
-                elif token == "- ":
-                    self.setStyling(length, Style.MCQ_INCORRECT_ANSWER)
-                elif token == "+ ":
-                    self.setStyling(length, Style.MCQ_CORRECT_ANSWER)
-                elif token == "! ":
-                    self.setStyling(length, Style.MCQ_NEUTRALIZED_ANSWER)
+                style = Style.PYTHON_BLOCK
+        else:
+            # print(repr(token))
+            if token.startswith("#"):
+                if token in TAGS:
+                    style = Style.PTYX_TAG
+                elif token == "#{":
+                    mode = Mode.EXPRESSION
+                    style = Style.PTYX_TAG
+                elif token.startswith("#") and token[1:].isalpha():
+                    style = Style.PTYX_VARIABLE
                 else:
-                    # Default style
-                    self.setStyling(length, Style.DEFAULT)
+                    style = Style.DEFAULT
+            elif token.startswith("\\") and token[1:].isalpha():
+                style = Style.PTYX_LATEX_MACRO
+            elif token == "- ":
+                style = Style.MCQ_INCORRECT_ANSWER
+            elif token == "+ ":
+                style = Style.MCQ_CORRECT_ANSWER
+            elif token == "! ":
+                style = Style.MCQ_NEUTRALIZED_ANSWER
+            else:
+                # Default style
+                style = Style.DEFAULT
+        return style, mode
